c4985159c515303dd48d6185a986b88b9b4088ea
iantheninja <munene.ian@live.com>
2017-02-15 00:04:44 +0800

    weather logic separated into front end and back end

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,17 +1,16 @@
-var apiKey = require('./../.env').apiKey;
+var Weather = require('./../js/weather.js').weatherModule;
+
+
+var displayHumidity = function(city, humidityData) {
+  $('.showWeather').text("The humidity in " + city + " is " + humidityData + "%");
+}
 
 $(document).ready(function() {
+  var weatherObj = new Weather();
+
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
-    .then(function(response){
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    })
-    .fail(function(error){
-      $('.showWeather').text(error.responseJSON.message);
-    });
-    console.log("Notice: the GET request has already been made");
+    weatherObj.getWeather(city, displayHumidity);
   });
 });
